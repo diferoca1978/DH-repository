32f6dbdaddb93fd74dc0a788953cec812293b49b
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2023-04-18 22:31:22 -0500

    modified ternary conditionals.js in 4conditionals and added  lookUpProfile.js to 7loops

--- module2/4conditionals/ternary conditional.js
+++ module2/4conditionals/ternary conditional.js
@@ -1,5 +1,16 @@
-//operador ternario que cumple la funcion como si fuera un (if - else)
+//operador ternario que cumple la funcion como si fuera un (if - else) But this kind of indentation is very confuse
 let edad = 35
 //             Condicion (if)                   Condicion (else-if)                Condicion (else)  
 let validacion = (edad > 40) ? 'eres mayor de 40' : (edad < 40) ? 'eres menor de 40' : 'tienes 40';
 console.log(validacion);
+
+//* this is de correct form to do that whe we need to use nested conditions ( nested if)
+
+function checkSign(num) {
+
+  return (num > 0) ? 'positive'
+  : (num < 0) ? 'negative'
+  : 'zero';
+  
+  }
+  checkSign(10);
