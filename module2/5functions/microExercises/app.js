ee395e7ffda89c333c676d836858c76b9d8100ef
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2023-04-20 19:58:46 -0500

    refactoring microChallenge2 of 5functions under dataHamburger.js and app.js / added pdf files to module3 wireframes

--- module2/5functions/microExercises/app.js
+++ module2/5functions/microExercises/app.js
@@ -3,18 +3,24 @@ let dataHamburger = require('./dataHamburger')
 
 let priceCalculation = {
   data : dataHamburger,
-  searchBurger : function (typeOfHamburguer){
-   
+  selectBurger : function (typeOfHamburguer, add1, add2, add3){
+    let total = 0
+    this.data.forEach(row => {
+      if (row.name == typeOfHamburguer) total += row.price
+      else if (row.name == add1) total += row.price
+      else if (row.name == add2) total += row.price
+      else if (row.name == add3) total += row.price
+      else 'No such burger'
+    }) 
+    return 'The total cost of burger is: $' + total
   },
-  searchAddition : function (...addition){
-    
-  }
 }
-
-console.log(priceCalculation.searchBurger('chicken')); 
+//let additionals = ['ham', 'cheese', 'mustard', 'tomato', 'lettuce', 'onion']
+console.log(priceCalculation.selectBurger('grillCocodrille','cheese', 'tomato', 'ham')); 
 //priceCalculation.searchBurger('chicken'); 
-//console.log(priceCalculation.searchAddition('ham')); 
-//priceCalculation.searchAddition('ham'); 
+//console.log(priceCalculation.selectAddition('ham', 'tomato' )); 
+//priceCalculation.selectAddition('ham', 'tomato'); 
+
 
 
 
--- module2/5functions/microExercises/dataHamburger.js
+++ module2/5functions/microExercises/dataHamburger.js
@@ -1,16 +1,13 @@
  let dataHamburgers = [
-  { 'steakMeat': 1800,
-   'chicken': 1500,
-   'vegetarian': 1200,
-  },
-  {  'ham': 30 ,
-     'cheese':25,
-     'mayonnaise':5,
-     'mustard':5,
-     'tomato':15,
-     'lettuce':15,
-     'onion': 10
-  }
+  { name : 'grillMeat', price : 1800 },
+  { name : 'grillChicken', price : 1500 },
+  { name : 'vegetarian', price : 1200 },
+  { name : 'ham', price : 30 },
+  { name : 'cheese', price : 25 },
+  { name : 'mustard', price : 5 },
+  { name : 'tomato', price : 15 },
+  { name : 'lettuce', price : 15 },
+  { name : 'onion', price : 10 }
 ]
 
 module.exports = dataHamburgers;
