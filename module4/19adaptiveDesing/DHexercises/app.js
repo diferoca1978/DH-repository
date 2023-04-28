98fd705977b6a92205f0351dbc828b5cf9606b8a
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2023-04-27 22:10:20 -0500

    added 19adativeDesing but it's incomplete

---
+++ module4/19adaptiveDesing/DHexercises/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const publicpath = path.resolve(__dirname, "./public");
+app.use(express.static(publicpath));
+
+app.listen(3000, () => {
+  console.log("server running in the 3000 port");
+});
+
+app.get("/", (req, res) => {
+  let htmlPath = path.resolve(__dirname, "./views/index.html");
+  res.sendFile(htmlPath);
+});
