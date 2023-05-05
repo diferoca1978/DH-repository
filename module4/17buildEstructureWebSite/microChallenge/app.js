61922293a9101f7c97ee71c2a2a23e87b5cb6026
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2023-05-04 22:00:25 -0500

    All done till 20AdvancePositioningFlexBox

--- module4/17buildEstructureWebSite/microChallenge/app.js
+++ module4/17buildEstructureWebSite/microChallenge/app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const publicpath = path.resolve(__dirname, "./public");
-app.use(express.static(publicpath));
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("server running in the 3000 port");
--- module4/project/app.js
+++ module4/project/app.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const publicpath = path.resolve(__dirname, "./public");
-app.use(express.static(publicpath));
+app.use(express.static("public"));
 
-app.listen(3030, () => {
-  console.log("server running in the 3030 port");
+app.listen(3000, () => {
+  console.log("server running in the 3000 port");
 });
 
 app.get("/", (req, res) => {
