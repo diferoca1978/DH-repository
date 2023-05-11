2b6080cba9d4e1d68a39fe2e24820f82d1be99f5
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2023-05-11 08:24:41 -0500

    index.html backgroundcolor grey

---
+++ module4/repoDemo/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/index.html"));
+});
+
+app.listen(3000, () => console.log("server running in the 3000 port"));
